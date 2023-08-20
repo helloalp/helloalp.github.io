55c7a0040e96ed664bcf297cee71bed9969d7b67
helloalp <102785380+helloalp@users.noreply.github.com>
2023-08-20 15:55:06 +0800

    Update content-management.js

--- content-management.js
+++ content-management.js
@@ -8,3 +8,11 @@ document.addEventListener('contextmenu', function(e) {
     e.preventDefault();
 });
 
+        document.addEventListener("copy", function (e) {
+            e.preventDefault();
+            var selectedText = window.getSelection().toString();
+            var copiedText = selectedText + "（NAU官网内容-侵权必究）";
+            e.clipboardData.setData("text/plain", copiedText);
+
+            alert("不允许复制内容，已添加来源信息。");
+    
