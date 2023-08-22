cf530603c78de9382d7607eff4cae54450c3de86
helloalp <102785380+helloalp@users.noreply.github.com>
2023-08-22 16:40:34 +0800

    Create aesscript.js

---
+++ aesscript.js
@@ -0,0 +1,18 @@
+const encryptButton = document.getElementById('encryptButton');
+const decryptButton = document.getElementById('decryptButton');
+const encryptedOutput = document.getElementById('encryptedOutput');
+const decryptedOutput = document.getElementById('decryptedOutput');
+
+encryptButton.addEventListener('click', () => {
+    const encryptInput = document.getElementById('encryptInput').value;
+    const encryptKey = document.getElementById('encryptKey').value;
+
+    
+});
+
+decryptButton.addEventListener('click', () => {
+    const decryptInput = document.getElementById('decryptInput').value;
+    const decryptKey = document.getElementById('decryptKey').value;
+
+    
+});
