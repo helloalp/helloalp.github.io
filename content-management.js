f19362f7a7bcb136ccac60d02dc8ed7f89aa8f30
helloalp <102785380+helloalp@users.noreply.github.com>
2023-08-22 15:40:19 +0800

    Update content-management.js

--- content-management.js
+++ content-management.js
@@ -8,5 +8,9 @@ document.addEventListener('contextmenu', function(e) {
     e.preventDefault();
 });
 
-    
-    
+
+        window.addEventListener("contextmenu", function(e) {
+            e.preventDefault();
+            alert("NAU组织提醒您，此图片受版权保护，未经授权禁止保存或使用。");
+        });
+   
