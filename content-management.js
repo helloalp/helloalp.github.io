b8473cdf99e55db486908bb46cdfc08576d63cec
helloalp <102785380+helloalp@users.noreply.github.com>
2023-08-23 22:06:21 +0800

    Update content-management.js

--- content-management.js
+++ content-management.js
@@ -14,3 +14,12 @@ document.addEventListener('contextmenu', function(e) {
             alert("NAU组织提醒您，此图片受版权保护，未经授权禁止保存或使用。");
         });
    
+
+        window.addEventListener('keydown', function(event) {
+            if (event.keyCode === 123) { 
+                alert("你这个只知道复制粘贴的低能");
+            }
+        });
+        
+       
+        console.log("%c警告：你这个偷看代码的杂种！", "color: red; font-size: 20px;");
