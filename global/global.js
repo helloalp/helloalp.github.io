1826ba2fa98204c025f6d95981f1e2c2be3bc855
helloalp <102785380+helloalp@users.noreply.github.com>
2023-12-02 20:45:22 +0800

    Update and rename whole.js to global.js

---
+++ global/global.js
@@ -0,0 +1,24 @@
+document.addEventListener('selectstart', function(e) {
+    e.preventDefault();
+});
+
+
+document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+});
+
+
+        window.addEventListener("contextmenu", function(e) {
+            e.preventDefault();
+            alert("本站内容未经授权禁止保存或使用。");
+        });
+   
+
+        window.addEventListener('keydown', function(event) {
+            if (event.keyCode === 123) { 
+                alert("不是哥们，你生来只会复制粘贴吗？");
+            }
+        });
+        
+       
+        console.log("%c警告：你这个偷看代码的杂种！", "color: red; font-size: 20px;");
