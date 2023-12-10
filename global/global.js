b78862f134528d4fb43d8a02d4750f6fc9cafff1
helloalp <102785380+helloalp@users.noreply.github.com>
2023-12-10 10:31:12 +0800

    Update global.js

--- global/global.js
+++ global/global.js
@@ -1,24 +1,30 @@
-document.addEventListener('selectstart', function(e) {
-    e.preventDefault();
-});
+// 检查是否存在名为antiCopy的标签在local storage中
+if (localStorage.getItem('antiCopy')) {
+    // 允许复制
+    document.addEventListener('copy', function(e) {
+        // 弹出提示框
+        alert("请珍惜你的复制许可，不要随意传播");
+    });
+} else {
+    // 防复制代码
+    document.addEventListener('selectstart', function(e) {
+        e.preventDefault();
+    });
 
+    document.addEventListener('contextmenu', function(e) {
+        e.preventDefault();
+    });
 
-document.addEventListener('contextmenu', function(e) {
-    e.preventDefault();
-});
+    window.addEventListener("contextmenu", function(e) {
+        e.preventDefault();
+        alert("本站内容未经授权禁止保存或使用。");
+    });
 
+    window.addEventListener('keydown', function(event) {
+        if (event.keyCode === 123) {
+            alert("不是哥们，你生来只会复制粘贴吗？");
+        }
+    });
 
-        window.addEventListener("contextmenu", function(e) {
-            e.preventDefault();
-            alert("本站内容未经授权禁止保存或使用。");
-        });
-   
-
-        window.addEventListener('keydown', function(event) {
-            if (event.keyCode === 123) { 
-                alert("不是哥们，你生来只会复制粘贴吗？");
-            }
-        });
-        
-       
-        console.log("%c警告：你这个偷看代码的杂种！", "color: red; font-size: 20px;");
+    console.log("%c警告：你这个偷看代码的狗杂种！", "color: red; font-size: 20px;");
+}
