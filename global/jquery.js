f6cdfc4ce05664fafe04e7a4c2bafb980d3d35f1
Francis Zhang <102785380+helloalp@users.noreply.github.com>
2024-06-29 11:04:13 +0800

    更新 jquery.js

--- global/jquery.js
+++ global/jquery.js
@@ -2,7 +2,7 @@
     
     var script = document.createElement('script');
     
-    script.src = 'https://cdn.example.com/library.js';
+    script.src = 'https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/jquery/3.6.0/jquery.js';
     
     document.head.appendChild(script);
 })();
