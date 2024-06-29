adfeb26a732b7fa81fa2f2c11c8b834010d9d250
Francis Zhang <102785380+helloalp@users.noreply.github.com>
2024-06-29 11:31:20 +0800

    Update jquery.js

--- global/jquery.js
+++ global/jquery.js
@@ -1,8 +1,15 @@
 (function() {
-    
     var script = document.createElement('script');
-    
     script.src = 'https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/jquery/3.6.0/jquery.js';
+    
+    script.onload = function() {
+        // jQuery 加载完成后再执行依赖代码
+        $(document).ready(function() {
+            $(".date").click(function() {
+                $(this).next(".content").slideToggle(500);
+            });
+        });
+    };
     
     document.head.appendChild(script);
 })();
