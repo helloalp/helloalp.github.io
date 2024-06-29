f45e425c3cf36ed71f5c838d8a1c625cf2f41a62
Francis Zhang <102785380+helloalp@users.noreply.github.com>
2024-06-29 11:02:50 +0800

    更新 jquery.js

--- global/jquery.js
+++ global/jquery.js
@@ -1,14 +1,8 @@
 (function() {
+    
     var script = document.createElement('script');
-    script.src = 'https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/jquery/3.6.0/jquery.js';
-    script.onload = function() {
-        console.log('CDN script loaded successfully.');
-    };
-    script.onerror = function() {
-        console.log('CDN script failed to load. Loading local script.');
-        var localScript = document.createElement('script');
-        localScript.src = '/personal/jquery/3.6.0/jquery.js';
-        document.head.appendChild(localScript);
-    };
+    
+    script.src = 'https://cdn.example.com/library.js';
+    
     document.head.appendChild(script);
 })();
